895b13b9bd46f956f2b5e19185b6af34d784f0fb
Henry Lomeu <hercuss.lomeu@gmail.com>
2022-11-30 09:03:27 -0300

    first commit

---
+++ src/data/categories.ts
@@ -0,0 +1,9 @@
+import { Category } from '../types/Category'
+
+export const categories: Category = {
+    food: {title: 'Alimentação', color:'red', expense: true},
+    //rent: {title: 'Aluguel', color:'red', expense: true},
+    leisure: {title: 'Lazer', color:'red', expense: true},
+    bill: {title: 'Contas', color:'red', expense: true},
+    revenue: {title: 'Receita', color:'green', expense: false},
+}
---
+++ src/data/items.ts
@@ -0,0 +1,9 @@
+import {Item} from '../types/Item'
+
+export const items = [
+    {date: new Date(2022, 7, 23), category: 'food', title: 'McDonalds', value: 31.12},
+    {date: new Date(2022, 7, 23), category: 'food', title: 'Burger King', value: 29.52},
+    {date: new Date(2022, 7, 24), category: 'salary', title: 'Salario', value: 4000},
+    {date: new Date(2022, 8, 18), category: 'rent', title: 'Aluguel', value: 2300}
+    
+];
